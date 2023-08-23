0038f79c2eac53d3d75173b3913708eaeed5f62c
chaehaeun <chae_3210@naver.com>
2023-08-23 11:55:29 +0900

    [⚡️성능] 이미지 최적화, 텍스트 압축을 통한 성능 개선

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import * as path from 'node:path'
 import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
+import { compression } from 'vite-plugin-compression2'
+import viteImagemin from '@vheemstra/vite-plugin-imagemin'
+import imageminGifSicle from 'imagemin-gifsicle'
+import imageminMozjpeg from 'imagemin-mozjpeg'
+import imageminPngQuant from 'imagemin-pngquant'
+import imageminSvgo from 'imagemin-svgo'
+import imageminWebp from 'imagemin-webp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +17,25 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [react(), checker({ typescript: true, overlay: true })],
+  plugins: [
+    react(),
+    checker({ typescript: true, overlay: true }),
+    compression(),
+    viteImagemin({
+      plugins: {
+        jpg: imageminMozjpeg(),
+        png: imageminPngQuant(),
+        gif: imageminGifSicle(),
+        svg: imageminSvgo(),
+      },
+      makeWebp: {
+        plugins: {
+          jpg: imageminWebp(),
+          png: imageminWebp(),
+        },
+      },
+    }),
+  ],
   server: {
     host: 'localhost',
     port: 3000,
