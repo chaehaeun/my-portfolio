1d04f6623d9159cd165c934dddb081857be1170b
chaehaeun <chae_3210@naver.com>
2023-09-05 00:17:30 +0900

    [💡] 쓸모없는 주석 삭제

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,8 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 const {
   VITE_FIREBASE_API_KEY,
@@ -22,6 +19,5 @@ const firebaseConfig = {
   appId: VITE_FIREBASE_APP_ID,
 }
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const dbService = getFirestore(app)
